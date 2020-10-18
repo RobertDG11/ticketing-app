ee13f1d1c26a836f038169b81d7757017d6f6b77
Deliu Robert <deliu.robert11@gmail.com>
2020-10-18 17:19:41 +0300

    Added expiration service and a new listener to orders service

---
+++ orders/src/events/listeners/__test__/expiration-complete-listener.test.ts
@@ -0,0 +1,76 @@
+import { natsWrapper } from '../../../nats-wrapper';
+import mongoose from 'mongoose';
+import { Message } from 'node-nats-streaming';
+import { Ticket } from '../../../models/ticket';
+import { ExpirationCompleteEvent, OrderStatus } from '@rdgtickets/common';
+import { ExpirationCompleteListener } from '../expiration-complete-listener';
+import { Order } from '../../../models/order';
+
+const setup = async () => {
+  // create an instance of the listener
+  const listener = new ExpirationCompleteListener(natsWrapper.client);
+
+  // Create and save a ticket
+  const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: 'concert',
+    price: 10,
+  });
+  await ticket.save();
+
+  // Create and save an order
+  const order = Order.build({
+    userId: new mongoose.Types.ObjectId().toHexString(),
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+    ticket,
+  });
+  await order.save();
+
+  // create a fake data event
+  const data: ExpirationCompleteEvent['data'] = {
+    orderId: order.id,
+  };
+
+  // create a fake message object
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, data, ticket, order, msg };
+};
+
+it('should update the order status to cancelled', async () => {
+  const { listener, data, msg, order } = await setup();
+
+  // call the onMessage function with the data object + message object
+  await listener.onMessage(data, msg);
+
+  const updatedOrder = await Order.findById(order.id);
+
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it('should ack the message', async () => {
+  const { listener, data, msg } = await setup();
+
+  // call the onMessage function with the data object + message object
+  await listener.onMessage(data, msg);
+
+  // write assertions to make sure ack function is called
+  expect(msg.ack).toHaveBeenCalled();
+});
+
+it('should emit an OrderCancelled event', async () => {
+  const { listener, data, msg, order } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+
+  const eventData = JSON.parse(
+    (natsWrapper.client.publish as jest.Mock).mock.calls[0][1]
+  );
+  expect(eventData.id).toEqual(order.id);
+});
